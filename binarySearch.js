06a963a33a6f5e04557db3349d1d4f65d79f34c4
Mike Hall <duke@michaeldukehall.com>
2019-08-15 18:24:45 -0700

    minor edits

--- binarySearch.js
+++ binarySearch.js
@@ -1,7 +1,12 @@
 function binarySearch(numArray, key) {
+    /* Recursive Solution: 
+    This function recursively calls itself until either: It has identified the key (base case) or the array length is 1 (key is not present).
+    Otherwise the function divides the array by half, pivoting on whether the key is less than or greater than the middle element.
+    */
     var middleIndex = Math.floor(numArray.length/2);
     var middleElement = numArray[middleIndex];
     var arrayLength = numArray.length - 1;
+
     print("middleIndex: " + middleIndex + " middleElement: " + middleElement + " Length: " + arrayLength);
 
     if (key === middleElement) return true;
@@ -14,7 +19,7 @@ function binarySearch(numArray, key) {
     else return false;
 }
 
-//print(binarySearch([1,2,3,4,5,6],4));
+//Test Cases
 print(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90],2));
 print(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90],80));
 print(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90],15));
