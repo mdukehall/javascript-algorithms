a9ae8740b301930596322fab1c92acd4c6bdd4bc
Mike Hall <duke@michaeldukehall.com>
2019-08-19 09:34:39 -0700

    added comments

--- reverseint.js
+++ reverseint.js
@@ -1,5 +1,13 @@
 function reverseInt(n) {
-    const reversed = n.toString().split('').reverse().join('');
+    /*
+    Algorithm: 
+        1. Convert the input variable to a string
+        2. split it into an array of characters
+        3. reverse the array
+        4. join in back into a string
+        5. convert it into an integer and multilpe the original sign of the input variable
+    */
+    let reversed = n.toString().split('').reverse().join('');
     print('reversed string: ' + reversed);
     return parseInt(reversed) * Math.sign(n);
 }
