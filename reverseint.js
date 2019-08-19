aaa6b638735794555441983edea639b3220564e8
Mike Hall <duke@michaeldukehall.com>
2019-08-19 11:33:25 -0700

    noting bug in parseInt()

--- reverseint.js
+++ reverseint.js
@@ -7,14 +7,25 @@ function reverseInt(n) {
         4. join in back into a string
         5. convert it into an integer and multilpe the original sign of the input variable
     */
+    let sign = Math.sign(n);
+    print('sign: ' + sign);
     let reversed = n.toString().split('').reverse().join('');
     print('reversed string: ' + reversed);
+    print('integer ' + parseInt(reversed));
     return parseInt(reversed) * Math.sign(n);
 }
 
 print(reverseInt(15));
 print(reverseInt(981));
+print(reverseInt(100));
+print(reverseInt(200));
+print(reverseInt(300));
+print(reverseInt(400));
 print(reverseInt(500));
+print(reverseInt(600));
+print(reverseInt(700));
+print(reverseInt(800));
+print(reverseInt(900));
 print(reverseInt(-15));
 print(reverseInt(-90));
-//this algorithm fails for values like -900
+//this algorithm fails for values 800, 900
