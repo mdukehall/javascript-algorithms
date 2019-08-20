149e77f92587176666656ba5acf9a6ebcb90f837
Mike Hall <duke@michaeldukehall.com>
2019-08-20 16:59:43 -0700

    adding reverse string

---
+++ reversestring.js
@@ -0,0 +1,14 @@
+function reverse1(str) {
+    //const arr = str.split('');
+    //arr.reverse();
+    //return arr.join(''); 
+    
+    return str.split('').reverse().join('');
+}
+
+function reverse2(str) {
+    return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+print(reverse1('killroywashere'));
+print(reverse2('killroywashere'));
