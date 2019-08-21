d2647e42825b0f810eb529d744e0fa26af7aeb7f
Mike Hall <duke@michaeldukehall.com>
2019-08-20 18:22:41 -0700

    reduced algorithm

---
+++ nthsmallestelement.js
@@ -0,0 +1,11 @@
+
+ function nth(arr, pos) {
+     /*
+    Problem: Given an array/list [] of integers , Find the Nth smallest element in this array of integers
+    */
+    let sorted = arr.sort((a,b) => a-b);
+    return sorted[pos-1];
+ }
+
+ print(nth([3,1,2] ,2));
+ print(nth([15,20,7,10,4,3] ,3));
