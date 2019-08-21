85be5c507d4464d0ce13fb5f5f48d4761ec00ab9
Mike Hall <duke@michaeldukehall.com>
2019-08-21 16:54:00 -0700

    tried mergesort, that doesn't work with repeats

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 /*
 Your goal in this kata is to implement a difference function, 
 which subtracts one list from another and returns the result.
@@ -5,21 +7,42 @@ It should remove all values from list a, which are present in list b.
 array_diff([1,2],[1]) == [2]
 array_diff([1,2,2,2,3],[2]) == [1,3]
 */
-function array_diff(a, b) {
+function mergeSort(array) {
+    //take in a single, unsorted array as a paramter
+    //split the array into two halves
+    if (array.length < 2) return array;
+    var middleIndex = Math.floor(array.length /2);
+    var firsthalf = array.slice(0,middleIndex);
+    var secondhalf = array.slice(middleIndex);
+    return merge(mergeSort(firsthalf), mergeSort(secondhalf));
+}
+
+function merge(array1, array2) {
+    //takes in two sorted arrays as parameters
+    //merges those sorted arrays into one sorted array
+    //returns one sorted array
     var result = [];
-    print('a: ' + a);
-    print('b: ' + b);
-    for (var i = 0; i < a.length ; i++) {
-        print('a[i]: ' + a[i] + ' is included in b: ' + b);
-        print('includes shows: ' + b.includes(a[i]));
-        if (b.includes(a[i]) == false) {
-            print('I am here');
-            result.push(a[i]);
-            print('result: ' + result);
+    while (array1.length && array2.length) {
+        var minElement;
+        if (array1[0] < array2[0]) minElement = array1.shift();
+        else minElement = array2.shift();
+        result.push(minElement);
+    }
+    if (array1.length) result = result.concat(array1);
+    else result = result.concat(array2);
+
+    return result;
+}
+
+function array_diff(a, b) {
+    let merged = mergeSort(a,b);
+    
+    for (var i = 0; i < merged.length ; i++) {
+        if (merged[i] === merged[i+1]) {
+            merged.splice(i,2);
         }
     }
-    print('result: ' + result);
-    return result;
+    return merged;
 }
 
 print(array_diff([1,2],[1]));
