583fcd024d89c27a0ea017e2bc5cc767aed0ec8a
Mike Hall <duke@michaeldukehall.com>
2019-08-25 10:55:11 -0700

    there's still a bug in the logic here

--- parserParens.js
+++ parserParens.js
@@ -4,17 +4,24 @@ and determines if the order of the parentheses is valid.
 The function should return true if the string is valid, and false if it's invalid.
 */
 function validParentheses(parens){
-    var left =  parens.split('').filter(function(leftp) {
-        return leftp == '(';
-    });
-    var right =  parens.split('').filter(function(rightp) {
-        return rightp == ')';
-    });
-    if (left.length === right.length) return true;
-    else return false;
+    var balance = 0;
+    //base case 1: array length must be greater than > 2 (otherwise you only have 1 paren)
+    if (parens.length <= 1) return 'balance: ' + balance + ' result: ' + false + ' failed base case 1';
+    //base case 2: must start with a left paren
+    if (parens[0] == ')') return 'balance: ' + balance + ' result: ' + false + ' failed base case 2';
+    //Ok, the string is ready to iterate
+    parens.split('').forEach(element => {
+        if (element == '(') balance++;
+        if (element == ')') balance--;
+        if (balance < 0) return 'balance: ' + balance + ' result: ' + false + ' too many right parens';
+      });
+      if (balance < 0 || balance > 0) return 'balance: ' + balance + ' result: ' + false + ' balance is off';
+      return 'balance: ' + balance + ' result: ' + true;
+    
   }
 
 print(validParentheses('()')); //true
 print(validParentheses(')(()))')); //false
 print(validParentheses('(')); //false
+print(validParentheses('())')); //false
 print(validParentheses('(())((()())())')); //true
