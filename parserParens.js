6ff6e426af56028058763b82e99b758bba6308cf
Mike Hall <duke@michaeldukehall.com>
2019-08-26 07:18:46 -0700

    still debugging parser

--- parserParens.js
+++ parserParens.js
@@ -1,23 +1,31 @@
+/*jshint esversion: 6 */
 /*
 Write a function called that takes a string of parentheses, 
 and determines if the order of the parentheses is valid. 
 The function should return true if the string is valid, and false if it's invalid.
 */
+let right = [];
+let left = [];
+let bal = 0;
+
+function lparen(str) {
+    left.push(str);
+    bal++;
+}
+function rparen(str) {
+    right.push(str);
+    bal--;
+}
 function validParentheses(parens){
-    var balance = 0;
-    //base case 1: array length must be greater than > 2 (otherwise you only have 1 paren)
-    if (parens.length <= 1) return 'balance: ' + balance + ' result: ' + false + ' failed base case 1';
-    //base case 2: must start with a left paren
-    if (parens[0] == ')') return 'balance: ' + balance + ' result: ' + false + ' failed base case 2';
-    //Ok, the string is ready to iterate
-    parens.split('').forEach(element => {
-        if (element == '(') balance++;
-        if (element == ')') balance--;
-        if (balance < 0) return 'balance: ' + balance + ' result: ' + false + ' too many right parens';
-      });
-      if (balance < 0 || balance > 0) return 'balance: ' + balance + ' result: ' + false + ' balance is off';
-      return 'balance: ' + balance + ' result: ' + true;
-    
+    const par = parens.split('');
+    par.forEach(element => {
+        if (element == '(') lparen(element);
+        if (element == ')') rparen(element);
+    });
+    print(left);
+    if (bal === 0) return true;
+    if (lparen.length === rparen.length) return true;
+    return false;
   }
 
 print(validParentheses('()')); //true
@@ -25,3 +33,4 @@ print(validParentheses(')(()))')); //false
 print(validParentheses('(')); //false
 print(validParentheses('())')); //false
 print(validParentheses('(())((()())())')); //true
+print(validParentheses('(())(((())())())')); //false
