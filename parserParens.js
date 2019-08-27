cbc654cca27518759ee472e97dd07a4e536fcf1a
Mike Hall <duke@michaeldukehall.com>
2019-08-27 06:40:14 -0700

    converting to class-based solution for tree

--- parserParens.js
+++ parserParens.js
@@ -4,33 +4,55 @@ Write a function called that takes a string of parentheses,
 and determines if the order of the parentheses is valid. 
 The function should return true if the string is valid, and false if it's invalid.
 */
-let right = [];
-let left = [];
-let bal = 0;
+class Tree {
+    constructor(tree, depth) {
+        //this will be a 2-dim array where the index is treated as the depth
+        this._tree=[];
+        this._depth=0;
+      }
+    
+    getDepth() {
+          return 
+      }
+    
+      addLeft(str) {
+        //evaluate the level and add the left paren to the tree
+        this._tree[];
+    }
 
-function lparen(str) {
-    left.push(str);
-    bal++;
-}
-function rparen(str) {
-    right.push(str);
-    bal--;
-}
+    addRight(str) {
+        //evaluate the level and add the right paren to the tree
+        this._tree.push(str);
+    }
+    
+  }
+  
+const tree = new Tree();
+
+
+
 function validParentheses(parens){
     const par = parens.split('');
     par.forEach(element => {
         if (element == '(') lparen(element);
-        if (element == ')') rparen(element);
+        if (element == ')') {
+            if (left.length == right.length) return false; // no parent lparen
+            rparen(element);
+        }
     });
-    print(left);
-    if (bal === 0) return true;
-    if (lparen.length === rparen.length) return true;
-    return false;
+
+    //Evaluate:
+    print('left:'+left+' right:'+right+' bal:'+bal);
+
+    if (bal != 0) return false;
+    if (lparen.length != rparen.length) return false;
+    return true;
   }
 
-print(validParentheses('()')); //true
-print(validParentheses(')(()))')); //false
-print(validParentheses('(')); //false
+//print(validParentheses('()')); //true
+//print(validParentheses(')(()))')); //false
+//print(validParentheses(')(')); //false
+//print(validParentheses('(')); //false
 print(validParentheses('())')); //false
-print(validParentheses('(())((()())())')); //true
-print(validParentheses('(())(((())())())')); //false
+//print(validParentheses('(())((()())())')); //true
+//print(validParentheses('(())(((())())())(')); //false
